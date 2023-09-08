4c99b2520b0632276df52d431e379635a369766b
AbrahamJuarezDeLaCruz <ajuarezc@proveedoresnmp.com.mx>
2023-09-08 00:01:09 -0600

    Se agrega configuración inicial del proyecto.

---
+++ src/main/java/com/kafka/udemy/app/MsApacheKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.udemy.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsApacheKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsApacheKafkaApplication.class, args);
+	}
+
+}
